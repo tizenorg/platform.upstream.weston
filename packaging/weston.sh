999702d3c6d32251483b01387c52cc6d5d8fbf52
Stephane Desneux <stephane.desneux@eurogiciel.fr>
2013-09-16 14:48:22 +0200

    Added DBUS_SESSION_BUS_ADDRESS in /etc/profile.d/weston.sh (Bug TIVI-1686 and other bugs related to dbus)
    Change-Id: I4a238174544999a5655d9a72a1b068a563d8bb31

--- packaging/weston.sh
+++ packaging/weston.sh
@@ -1,2 +1,5 @@
 export ELM_ENGINE=wayland_egl
 export ECORE_EVAS_ENGINE=wayland_egl
+
+# also export dbus session address for dbus clients (details on bug TIVI-1686 [https://bugs.tizen.org/jira/browse/TIVI-1686])
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$UID/dbus/user_bus_socket
