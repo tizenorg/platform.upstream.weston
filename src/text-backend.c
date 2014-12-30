01751a8cc982a362b09058f546d8856e0eb883b1
Imran Zaman <imran.zaman@intel.com>
2014-12-30 15:20:13 +0200

    Enable/disable default virtual keyboard
    Since fullscreen shell does not support virtual keyboard,
    added configure option to enable/disable default virtual
    keyboard. This avoids unnecessary trial of launching vkb
    and hence will improve boot time
    
    Change-Id: I67bdd8ee231d101d6e3747a45968758af42ebc5a
    Bug-Tizen: TC-810
    Signed-off-by: Imran Zaman <imran.zaman@intel.com>

--- src/text-backend.c
+++ src/text-backend.c
@@ -969,15 +969,18 @@ static void
 text_backend_configuration(struct text_backend *text_backend)
 {
 	struct weston_config_section *section;
-	char *client;
-	int ret;
+	char *client = NULL;
+	int ret = -1;
 
 	section = weston_config_get_section(text_backend->compositor->config,
 					    "input-method", NULL, NULL);
+#if HAVE_DEFAULT_VKB
 	ret = asprintf(&client, "%s/weston-keyboard",
 		       weston_config_get_libexec_dir());
+#endif
 	if (ret < 0)
 		client = NULL;
+
 	weston_config_section_get_string(section, "path",
 					 &text_backend->input_method.path,
 					 client);
